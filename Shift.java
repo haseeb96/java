52d48ed89d101e8e986a0a93d1bff549ec8a52f8
Haseeb <ahamedhaseeb0206@gmail.com>
2016-09-02 11:58:57 +0530

    Shift.java

---
+++ Shift.java
@@ -0,0 +1,25 @@
+# java
+import java.util.*;
+class Shift{
+public static void main(String[] args){
+Scanner nr=new Scanner(System.in);
+StringBuffer s=new StringBuffer(nr.next);
+s.reverse();
+String output1=s.toString();
+String output2=output1;
+for(int i=0;i<output2.length();i++)
+{
+char a=output2.charAt(i);
+for(int j=i+1;j<output2.length();j++)
+{
+char b=output2.charAt(j);
+if(a==b)
+{
+output2.charAt(j)=='';
+}
+}
+}
+System.out.println(output1);
+System.out.println(output2);
+}
+}
